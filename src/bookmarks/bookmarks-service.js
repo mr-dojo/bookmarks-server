9d19cb0d1802a2f77fd3dfc9dd92731f0f98f87a
Carlo Costantino <costantinoart@gmail.com>
2020-02-07 18:15:45 +0000

    Added insert, delete, and update methods

--- src/bookmarks/bookmarks-service.js
+++ src/bookmarks/bookmarks-service.js
@@ -10,6 +10,25 @@ const BookmarksService = {
       .where('id', id)
       .first()
   },
+  insertBookmark(knexInstance, newBookmark) {
+    return knexInstance
+      .insert(newBookmark)
+      .into('bookmarks')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  deleteBookmark(knexInstance, id) {
+    return knexInstance('bookmarks')
+      .where({ id })
+      .delete()
+  },
+  updateBookmark(knexInstance, id, newBookmarkFields) {
+    return knexInstance('bookmarks')
+      .where({ id })
+      .update(newBookmarkFields)
+  },
 }
 
-module.exports = BookmarksService;
+module.exports = BookmarksService
